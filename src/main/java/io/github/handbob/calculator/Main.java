0a3d705b67498b0b05a40a6c7422701b46ad3b5d
handbob <handbob@icloud.com>
2023-01-29 20:23:38 +0100

    change project

---
+++ src/main/java/io/github/handbob/calculator/Main.java
@@ -0,0 +1,8 @@
+package io.github.handbob.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        String message = "Hello, world!";
+        System.out.printf("%s\n", message);
+    }
+}
