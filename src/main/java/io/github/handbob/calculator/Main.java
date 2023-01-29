8af8284032b9264f1c101365e0a8d463ea325e1f
handbob <handbob@icloud.com>
2023-01-29 21:57:53 +0100

    change project

--- src/main/java/io/github/handbob/calculator/Main.java
+++ src/main/java/io/github/handbob/calculator/Main.java
@@ -1,8 +1,71 @@
 package io.github.handbob.calculator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello, world!";
-        System.out.printf("%s\n", message);
+        try (Scanner scanner = new Scanner(System.in)) {
+            String message = new String();
+            String option = new String();
+            Operators operators = new Operators();
+            String inputA = new String();
+            String inputB = new String();
+            
+            message = "Interactive commandline calculator!\nchoose one option:\n1. calculator\n2. exit\noption: ";
+
+            System.out.printf("%s", message);
+            option = scanner.nextLine();
+            switch (option) {
+                case "1":
+                    message = "Operators:\n1. addition\n2. subtraction\n3. multiplication\n4. division\n5. exit\noption: ";
+                    System.out.printf("%s", message);
+                    option = scanner.nextLine();
+                    switch (option) {
+                        case "1":
+                            message = "Enter first number: ";
+                            System.out.printf("%s", message);
+                            inputA = scanner.nextLine();
+                            message = "Enter second number: ";
+                            System.out.printf("%s", message);
+                            inputB = scanner.nextLine();
+                            System.out.printf("%.2f\n", operators.addition(Double.parseDouble(inputA), Double.parseDouble(inputB)));
+                            break;
+                        case "2":
+                            message = "Enter first number: ";
+                            System.out.printf("%s", message);
+                            inputA = scanner.nextLine();
+                            message = "Enter second number: ";
+                            System.out.printf("%s", message);
+                            inputB = scanner.nextLine();
+                            System.out.printf("%.2f\n", operators.addition(Double.parseDouble(inputA), Double.parseDouble(inputB)));
+                            break;
+                        case "3":
+                            message = "Enter first number: ";
+                            System.out.printf("%s", message);
+                            inputA = scanner.nextLine();
+                            message = "Enter second number: ";
+                            System.out.printf("%s", message);
+                            inputB = scanner.nextLine();
+                            System.out.printf("%.2f\n", operators.addition(Double.parseDouble(inputA), Double.parseDouble(inputB)));
+                            break;
+                        case "4":
+                            message = "Enter first number: ";
+                            System.out.printf("%s", message);
+                            inputA = scanner.nextLine();
+                            message = "Enter second number: ";
+                            System.out.printf("%s", message);
+                            inputB = scanner.nextLine();
+                            System.out.printf("%.2f\n", operators.addition(Double.parseDouble(inputA), Double.parseDouble(inputB)));
+                            break;
+                        case "5":
+                            message = "Thank you for using calculator!";
+                            System.out.printf("%s\n", message);
+                            break;
+                        }
+                    break;
+                case "2":
+                    break;
+            }
+        }
     }
 }
